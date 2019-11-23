69bff8d4896fa09ad7c21d7fe3ed58b30790653f
Alberto Femenías Hermida <56123138+AlbertoFemenias@users.noreply.github.com>
2019-11-23 22:19:27 +0100

    Add files via upload

---
+++ assets/js/identicon.js
@@ -0,0 +1,106 @@
+var array = [[],[],[]];
+var colors = ['#C0392B','#E74C3C','#9B59B6','#8E44AD','#2980B9','#3498DB','#1ABC9C','#27AE60','#16A085','#F4D03F','#F39C12','#E67E22','#D35400','#BDC3C7','#95A5A6','#2C3E50'];
+var c;
+var cc;
+
+
+window.onload = ()=> {
+  c=document.getElementById("mcanvas");
+  cc=c.getContext("2d");
+  generateFromWord();
+}
+
+
+function generateFromWord () {
+  palabra = document.getElementById("inputText").value ;
+  generateIcon(palabra);
+
+}
+
+
+function generateRandom () {
+  palabra =  Math.random().toString(36).substring(2, 9) + Math.random().toString(36).substring(2, 9);
+  generateIcon(palabra);
+  document.getElementById("inputText").value = palabra;
+
+}
+
+
+function generateIcon (palabra) {
+
+  cc.fillStyle = 'white';
+  cc.fillRect(0, 0, 200, 200);
+
+  hasheado = hash(palabra);
+
+  var bitarray = [];
+
+  for(var i = 0; i < 32; ++i){
+    bitarray[i] = (hasheado >> i) & 1;
+  }
+
+  for (var j = 0; j < 3; j++){
+    for (var i = 0; i < 5; i++) {
+      array[j][i] = bitarray[(4*i)+j];
+    }
+  }
+
+  color = colors[hasheado % (colors.length-1)]
+
+  iconPaintRow(array, color);
+  
+  /* cc.fillStyle = 'black';
+  cc.font = '10px Arial';
+  cc.fillText("Input :"+ palabra, 20, 20);
+  cc.fillText("Hash :"+ hasheado, 20, 40);
+  cc.fillText("Arr :"+ bitarray, 20, 60); */
+
+}
+
+
+function hash(s) {
+    /* Simple hash function. */
+    var a = 1, c = 0, h, o;
+    if (s) {
+        a = 0;
+        /*jshint plusplus:false bitwise:false*/
+        for (h = s.length - 1; h >= 0; h--) {
+            o = s.charCodeAt(h);
+            a = (a<<6&268435455) + o + (o<<14);
+            c = a & 266338304;
+            a = c!==0?a^c>>21:a;
+        }
+    }
+    return String(a);
+};
+
+
+function iconPaintRow( array, color) {
+
+  cc.fillStyle = color;
+  
+  for(var j = 0; j < 3; j++){
+    for (var i = 0; i < 5; i++) {
+      if (array[j][i])
+        cc.fillRect(40*j, i*40, 40, 40);
+    }
+  }
+
+  for(var j = 0; j < 3; j++){
+    for (var i = 0; i < 5; i++) {
+      if (array[j][i])
+        cc.fillRect(160-(40*j), i*40, 40, 40);
+    }
+  }  
+
+}
+
+
+function download_image(){
+  var canvas = document.getElementById("mcanvas");
+  image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+  var link = document.createElement('a');
+  link.download = "identicon.png";
+  link.href = image;
+  link.click();
+}
